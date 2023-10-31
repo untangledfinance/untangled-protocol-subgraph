2a9c3ef03f16f566ad7ac49ceab22f1e435f2b56
vjtc0n <vjtc0n12345@gmail.com>
2023-10-31 21:33:27 +0700

    Update

---
+++ src/mappings/securitization-manager.ts
@@ -0,0 +1,6 @@
+import { Address, log } from '@graphprotocol/graph-ts';
+import { NewPoolCreated } from '../../generated/SecuritizationManager/SecuritizationManager';
+
+export function handlePoolCreated(event: NewPoolCreated): void {
+  log.info('handlePoolCreated', [event.params.instanceAddress.toString()]);
+}
