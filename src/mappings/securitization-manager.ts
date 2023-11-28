a1cc7df6a5d7a066a6ee74d8c77260138cbfcbac
haupc <haupc.uet@gmail.com>
2023-11-28 08:52:21 +0700

    basic mapping

--- src/mappings/securitization-manager.ts
+++ src/mappings/securitization-manager.ts
@@ -1,8 +1,81 @@
-import { Address, log } from '@graphprotocol/graph-ts';
-import { NewPoolCreated } from '../../generated/SecuritizationManager/SecuritizationManager';
+import {
+  Initialized as InitializedEvent,
+  NewNotesTokenCreated as NewNotesTokenCreatedEvent,
+  NewPoolCreated as NewPoolCreatedEvent,
+  NewTGECreated as NewTGECreatedEvent,
+  Paused as PausedEvent,
+  RoleAdminChanged as RoleAdminChangedEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent,
+  TokensPurchased as TokensPurchasedEvent,
+  Unpaused as UnpausedEvent
+} from "../../generated/SecuritizationManager/SecuritizationManager"
+import {
+  Initialized,
+  NewNotesTokenCreated,
+  NewPoolCreated,
+  NewTGECreated,
+  Paused,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  TokensPurchased,
+  Unpaused
+} from "../../generated/schema"
 
-export function handlePoolCreated(event: NewPoolCreated): void {
-  log.info(`handlePoolCreated {}`, [
-    event.params.instanceAddress.toHexString(),
-  ]);
+export function handleNewNotesTokenCreated(
+  event: NewNotesTokenCreatedEvent
+): void {
+  let entity = new NewNotesTokenCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.instanceAddress = event.params.instanceAddress
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleNewPoolCreated(event: NewPoolCreatedEvent): void {
+  let entity = new NewPoolCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.instanceAddress = event.params.instanceAddress
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleNewTGECreated(event: NewTGECreatedEvent): void {
+  let entity = new NewTGECreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.instanceAddress = event.params.instanceAddress
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTokensPurchased(event: TokensPurchasedEvent): void {
+  let entity = new TokensPurchased(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.investor = event.params.investor
+  entity.tgeAddress = event.params.tgeAddress
+  entity.amount = event.params.amount
+  entity.tokenAmount = event.params.tokenAmount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
 }
