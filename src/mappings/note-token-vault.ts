2e4c02fbfb694d117e700720be43cd99436a971d
tanlm <luuminhtan046@gmail.com>
2024-01-30 00:48:01 +0700

    [Feature] Epoch withdraw: cancel order

--- src/mappings/note-token-vault.ts
+++ src/mappings/note-token-vault.ts
@@ -1,11 +1,12 @@
-import {RedeemOrder as RedeemOrderEvent} from "../../generated/NoteTokenVault/NoteTokenVault";
+import {RedeemOrder as RedeemOrderEvent, CancelOrder as CancelOrderEvent} from "../../generated/NoteTokenVault/NoteTokenVault";
 import {RedeemOrder} from "../../generated/schema";
+import {BigInt} from "@graphprotocol/graph-ts";
 
 
 export function handleRedeemOrder(event: RedeemOrderEvent): void {
-    let order = RedeemOrder.load(event.transaction.hash.concatI32(event.logIndex.toI32()))
+    let order = RedeemOrder.load(event.params.pool.toHexString().concat(event.params.noteTokenAddress.toHexString()).concat(event.params.usr.toHexString()))
     if (!order) {
-        order = new RedeemOrder(event.transaction.hash.concatI32(event.logIndex.toI32()))
+        order = new RedeemOrder(event.params.pool.toHexString().concat(event.params.noteTokenAddress.toHexString()).concat(event.params.usr.toHexString()))
     }
     order.pool = event.params.pool.toHexString()
     order.noteTokenAddress = event.params.noteTokenAddress
@@ -15,5 +16,21 @@ export function handleRedeemOrder(event: RedeemOrderEvent): void {
     order.createdTimestamp = event.block.timestamp
     order.createdBlockNumber = event.block.number
     order.createdTransactionHash = event.transaction.hash.toHexString()
+    order.save()
+}
+
+export function handleRedeemOrder(event: CancelOrderEvent): void {
+    let order = RedeemOrder.load(event.params.pool.toHexString().concat(event.params.noteTokenAddress.toHexString()).concat(event.params.usr.toHexString()))
+    if (!order) {
+        order = new RedeemOrder(event.params.pool.toHexString().concat(event.params.noteTokenAddress.toHexString()).concat(event.params.usr.toHexString()))
+    }
+    order.pool = event.params.pool.toHexString()
+    order.noteTokenAddress = event.params.noteTokenAddress
+    order.user = event.params.usr
+    order.noteTokenRedeemAmount = null;
+    order.noteTokenPrice = null;
+    order.createdTimestamp = null;
+    order.createdBlockNumber = null;
+    order.createdTransactionHash = null;
     order.save()
 }
