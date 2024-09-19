8a5507c51b2ee4b48d5f2656e4d5caf9771778a5
tanlm <luuminhtan046@gmail.com>
2024-09-19 22:37:02 +0700

    Vault: vault investment token added transaction

--- src/mappings/vault.ts
+++ src/mappings/vault.ts
@@ -1,20 +1,7 @@
-import {Deposit, Drawdown, Withdraw} from "../../generated/Vault/Vault";
-import {VaultActivity} from "../../generated/schema";
+import {Deposit, Withdraw, TokenAdded} from "../../generated/Vault/Vault";
+import {UserPoolInvestment, VaultActivity, VaultInvestment} from "../../generated/schema";
+import {BigInt} from "@graphprotocol/graph-ts";
 
-
-export function handleDrawdown(event: Drawdown): void {
-    let vaultActivity = VaultActivity.load(event.transaction.hash.concatI32(event.logIndex.toI32()));
-    if (!vaultActivity) {
-        vaultActivity = new VaultActivity(event.transaction.hash.concatI32(event.logIndex.toI32()));
-    }
-    vaultActivity.transactionType = "MANAGER_DRAWDOWN";
-    vaultActivity.amount = event.params.amount;
-    vaultActivity.from = event.transaction.from.toHexString();
-    vaultActivity.createdTimestamp = event.block.timestamp;
-    vaultActivity.createdBlockNumber = event.block.number;
-    vaultActivity.createdTransactionHash = event.transaction.hash.toHexString();
-    vaultActivity.save();
-}
 
 export function handleDeposit(event: Deposit): void {
     let vaultActivity = VaultActivity.load(event.transaction.hash.concatI32(event.logIndex.toI32()));
@@ -42,4 +29,14 @@ export function handleWithdraw(event: Withdraw): void {
     vaultActivity.createdBlockNumber = event.block.number;
     vaultActivity.createdTransactionHash = event.transaction.hash.toHexString();
     vaultActivity.save();
+}
+
+export function handleTokenAdded(event: TokenAdded): void {
+    let vaultInvestment = VaultInvestment.load(event.params.chainId.toHexString().concat(event.params.tokenAddress.toHexString()));
+    if (!vaultInvestment) {
+        vaultInvestment = new VaultInvestment(event.params.chainId.toHexString().concat(event.params.tokenAddress.toHexString()));
+    }
+    vaultInvestment.tokenAddress = event.params.tokenAddress.toHexString();
+    vaultInvestment.chainId = event.params.chainId;
+    vaultInvestment.save();
 }
